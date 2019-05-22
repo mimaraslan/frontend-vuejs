a043fc6dadadb149d4a4371a799b9cc038c3f9bb
Mimar Aslan <mimaraslan@gmail.com>
2019-05-22 10:17:19 +0300

    VueJS
    VueJS

---
+++ _003_v-html/app.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", () => {
+   vue = new Vue({
+        el: '#app',
+        data: {
+          message: '<h1>Hello VueJS!</h1>',
+          username: "Mimar Aslan"
+        }
+      })
+} ) 
